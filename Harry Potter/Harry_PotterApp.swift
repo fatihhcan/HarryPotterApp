b7e08433b3c524cf3b2aca6724e0ca23254f218c
fatihhcan <dev.fatihcan@gmail.com>
2024-11-12 22:20:13 +0300

    Initial Commit

---
+++ Harry Potter/Harry_PotterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Harry_PotterApp.swift
+//  Harry Potter
+//
+//  Created by Fatih Can on 12.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct Harry_PotterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
